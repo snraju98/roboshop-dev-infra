bd46f64168e17df15f9c007b83d41c2f773e0f7d
snraju98 <nagarajusagiraju123@gmail.com>
2026-06-27 11:45:31 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -76,3 +76,42 @@ resource "terraform_data" "redis" {
     ]
   }
 }
+
+resource "aws_instance" "rabbitmq" {
+  ami           = data.aws_ami.joindevops.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+  subnet_id = local.database_subnet_id
+  
+  tags = merge(
+    {
+        Name = "${local.common_name}-rabbitmq"
+    },
+    local.common_tags
+  )
+}
+
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id
+  ]
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    password = "DevOps321"
+    host        = aws_instance.rabbitmq.private_ip
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh rabbitmq ${var.environment}"
+    ]
+  }
+}
--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -14,3 +14,11 @@ resource "aws_route53_record" "redis" {
   ttl     = 1
   records = [aws_instance.redis.private_ip]
 }
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = var.zone_id
+  name    = "rabbitmq-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.rabbitmq.private_ip]
+}
