90aec50e36053efbe563fc581c80aeb0a4f06e35
snraju98 <nagarajusagiraju123@gmail.com>
2026-06-27 10:26:00 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-component=$1
-environment=$2
+component=$1 #mongodb
+environment=$2 #dev
 dnf install ansible -y
 mkdir -p /var/log/roboshop/
 chmod -R ec2-user:ec2-user /var/log/roboshop
@@ -10,6 +10,6 @@ touch /var/log/roboshop/ansible.log
 
 cd /home/ec2-user
 git clone https://github.com/snraju98/roboshop-ansible-v3.git
-cd roboshop-ansible-v3.git
+cd roboshop-ansible-v3
 git pull
 ansible-playbook -e component=$component -e env=$environment roboshop.yaml
