a13df70b3e2fee716632d4de6d5e89ac3a37a5ca
snraju98 <nagarajusagiraju123@gmail.com>
2026-06-30 15:12:07 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,6 +1,5 @@
 locals {
     catalogue_alb_sg_id = data.aws_ssm_parameter.catalogue_alb_sg_id.value
-    ami_id = data.aws_ami.joindevops.id
     common_name = "${var.project}-${var.environment}"
     private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
     common_tags = {
