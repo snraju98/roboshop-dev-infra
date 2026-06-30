7030f56963ef7de6cbb9dce8dda25bdf32d843ee
snraju98 <nagarajusagiraju123@gmail.com>
2026-06-30 14:25:12 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -11,3 +11,28 @@ resource "aws_instance" "catalogue" {
     local.common_tags
   )
 }
+
+resource "terraform_data" "catalogue" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    password = "DevOps321"
+    host        = aws_instance.catalogue.private_ip
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh catalogue ${var.environment} ${var.app_version}"
+    ]
+  }
+}
