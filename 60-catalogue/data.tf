b0a7e0588e023999ec0411c3daccaea6503cab4c
snraju98 <nagarajusagiraju123@gmail.com>
2026-06-30 15:33:18 +0530

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -8,6 +8,10 @@ data "aws_ssm_parameter" "private_subnet_ids" {
     name = "/${var.project}/${var.environment}/private_subnet_ids"
 }
 
+data "aws_ssm_parameter" "vpc_id" {
+    name = "/${var.project}/${var.environment}/vpc_id"
+}
+
 data "aws_ami" "joindevops" {
   most_recent      = true
   owners           = ["973714476881"]
