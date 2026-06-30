a4db136b61c1e637d1d47fd9bd132aad71f3fb57
snraju98 <nagarajusagiraju123@gmail.com>
2026-06-30 16:10:45 +0530

    terraform

--- 60-catalogue/bootstrap.sh
+++ 60-catalogue/bootstrap.sh
@@ -13,4 +13,4 @@ cd /home/ec2-user
 git clone https://github.com/snraju98/roboshop-ansible-v3.git
 cd roboshop-ansible-v3
 git pull
-ansible-playbook -e component=$component -e env=$environment roboshop.yaml
+ansible-playbook -e component=$component -e env=$environment -e app_version=$app_version roboshop.yaml
--- 60-catalogue/variables.tf
+++ 60-catalogue/variables.tf
@@ -6,6 +6,10 @@ variable "environment" {
     default = "dev"
 }
 
+variable "app_version" {
+    default = "v3"
+}
+
 /* variable "zone_id" {
     default = "Z04059211037V5XD2IM6D"
 }  
