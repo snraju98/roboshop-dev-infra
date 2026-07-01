8afe791e8d3093ab1f3672a66d4c239e0ec3c9f3
snraju98 <nagarajusagiraju123@gmail.com>
2026-07-01 16:28:23 +0530

    terraform

--- 60-catalogue/variables.tf
+++ 60-catalogue/variables.tf
@@ -10,11 +10,11 @@ variable "app_version" {
     default = "v3"
 }
 
-/* variable "zone_id" {
+/*  variable "zone_id" {
     default = "Z04059211037V5XD2IM6D"
-}  
+}  */ 
 
 variable "domain_name" {
     default = "daws90s.fun"
 }
- */
+ 
