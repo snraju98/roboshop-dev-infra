fa969d3544366dfd25102b1e39437ed456aee3c3
snraju98 <nagarajusagiraju123@gmail.com>
2026-07-02 15:22:25 +0530

    terraform

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "daws90s.fun"
+}
+
+ variable "zone_id" {
+    default = "Z04059211037V5XD2IM6D"
+} 
--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "components" {
     for_each = var.components
-    source = "../../terraform-roboshop-component"
+    source = "https://github.com/snraju98/terraform-roboshop-component.git?ref=main"
     environment = var.environment
     component = each.key
     app_version = each.value.app_version
